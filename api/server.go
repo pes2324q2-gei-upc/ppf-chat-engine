aceb63c2f1b9f98da7ac73a662e9b646207ab9b5
pg-goose <pau.galopa.dev@gmail.com>
2024-04-09 13:33:40 +0200

    some refactoring

--- api/server.go
+++ api/server.go
@@ -4,36 +4,42 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/gorilla/websocket"
+	"github.com/gorilla/mux"
 	"github.com/pes2324q2-gei-upc/ppf-chat-engine/chat"
 )
 
+type ChatApiController struct {
+	engine *chat.ChatEngine
+}
+
 // RegisterHandlers registers the HTTP request handlers.
-func RegisterHandlers() {
+func (controller *ChatApiController) RegisterHandlers() {
 	log.Println("Registering handlers")
-	http.HandleFunc("/", RootHandler)
-	http.HandleFunc("/ws", WsHandler)
+	http.HandleFunc("/", controller.RootHandler)
+	http.HandleFunc("/connect/<userId>", controller.DefaultConnectHandler)
 }
 
 // RootHandler handles the root HTTP request, always sends 200 status code and an empty body.
-func RootHandler(w http.ResponseWriter, r *http.Request) {
+func (controller *ChatApiController) RootHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 }
 
 // WsHandler handles the WebSocket request.
-func WsHandler(w http.ResponseWriter, r *http.Request) {
-	ServeWs(chat.DefaultChatEngine, w, r)
+func (controller *ChatApiController) DefaultConnectHandler(w http.ResponseWriter, r *http.Request) {
+	id := chat.UserId(mux.Vars(r)["userId"])
+	// If the user does not exist on the engine, load it.
+	if !controller.engine.Exists(chat.UserId(id)) {
+		if err := controller.engine.LoadUser(id); err != nil {
+			http.Error(w, err.Error(), http.StatusNotFound)
+			return
+		}
+	}
+	if err := controller.engine.ConnectUser(id, w, r); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
 
-// ServeWs upgrades the HTTP connection to a WebSocket connection and creates a new server client for the connection.
-func ServeWs(engine chat.ChatEngine, w http.ResponseWriter, r *http.Request) {
-	var upgrader = websocket.Upgrader{}
-	conn, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	client := chat.NewClient(conn)
-	engine.RegisterClient(client)
-	log.Println("New client connected")
-}
+// POST /user/<userId>/join/<roomId>
+// POST /user/<userId>/leave/<roomId>
+//
+// POST /room
